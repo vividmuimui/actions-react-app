e853b419b92277b167fc8b7630480f16ea9eed07
vividmuimui <1803598+vividmuimui@users.noreply.github.com>
2019-10-23 01:30:23 +0900

    Notify coverage report url

--- .circleci/danger/dangerfile.ts
+++ .circleci/danger/dangerfile.ts
@@ -1,4 +1,10 @@
 import { danger, markdown, message, fail, warn } from "danger"
-import storyBookUrl from './storybook'
+
+const buidNum = process.env.CIRCLE_BUILD_NUM
+if (buidNum === undefined) throw new Error('CIRCLE_BUILD_NUM is undefined')
+
+const coverageUrl = `https://${buidNum}-216242872-gh.circle-artifacts.com/0/tmp/coverage/lcov-report/index.html`
+const storyBookUrl = `https://${buidNum}-216242872-gh.circle-artifacts.com/0/tmp/storybook/index.html`
 
 markdown(`- :book: [StoryBook](${storyBookUrl})`)
+markdown(`- :chart_with_upwards_trend: [Coverage](${coverageUrl})`)
--- .circleci/danger/storybook.ts
+++
@@ -1,5 +0,0 @@
-const buidNum = process.env.CIRCLE_BUILD_NUM
-if (buidNum === undefined) throw new Error('CIRCLE_BUILD_NUM is undefined')
-
-const storyBookUrl = `https://${buidNum}-216242872-gh.circle-artifacts.com/0/tmp/storybook/index.html`
-export default storyBookUrl
