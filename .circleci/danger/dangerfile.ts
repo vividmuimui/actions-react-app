7d0cdaba8efa505db477dd2db379f891f8f4999d
vividmuimui <1803598+vividmuimui@users.noreply.github.com>
2019-10-23 02:05:04 +0900

    Split job
    https://circleci.com/docs/ja/2.0/workflows/#ジョブ間のデータ共有を可能にする-workspaces-を使う
    https://github.com/circleci/circleci-docs/blob/master/.circleci/config.yml

--- .circleci/danger/dangerfile.ts
+++ .circleci/danger/dangerfile.ts
@@ -3,8 +3,9 @@ import { danger, markdown, message, fail, warn } from "danger"
 const buidNum = process.env.CIRCLE_BUILD_NUM
 if (buidNum === undefined) throw new Error('CIRCLE_BUILD_NUM is undefined')
 
-const coverageUrl = `https://${buidNum}-216242872-gh.circle-artifacts.com/0/tmp/coverage/lcov-report/index.html`
-const storyBookUrl = `https://${buidNum}-216242872-gh.circle-artifacts.com/0/tmp/storybook/index.html`
+const baseUrl = `https://${buidNum}-216242872-gh.circle-artifacts.com/0/tmp/workspace`
+const coverageUrl = `${baseUrl}/coverage/lcov-report/index.html`
+const storyBookUrl = `${baseUrl}/storybook/index.html`
 
 markdown(`- :book: [StoryBook](${storyBookUrl})`)
 markdown(`- :chart_with_upwards_trend: [Coverage](${coverageUrl})`)
